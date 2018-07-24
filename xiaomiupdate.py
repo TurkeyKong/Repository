3f0b38ec476da46bc946ceea9bde62b0d52ce140
TurkeyKong <TurkeyKong@github.com>
2018-07-24 22:20:11 +0800

    xiaomi-update.py

---
+++ xiaomiupdate.py
@@ -0,0 +1,12 @@
+# -*- conding:utf-8 -*-
+
+import os
+
+cmd='python switch-version_stress.py 192.168.151.51 dolphinzorro gongdejies@allwinnertech.com'
+
+main():
+	print('python switch-version_stress.py 192.168.151.51 dolphinzorro gongdejies@allwinnertech.com')
+	os.system(cmd)
+
+if __name__ == '__main__':
+	main()
